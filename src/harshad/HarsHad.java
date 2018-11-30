50c91e87b947d358e1b8f80df8a95fa7f2b85c4b
István Molnár <molnar.istvan@workmail.hu>
2018-11-30 11:00:45 +0100

    Ez egy teszt

---
+++ src/harshad/HarsHad.java
@@ -0,0 +1,28 @@
+package harshad;
+
+import java.util.Scanner;
+
+public class HarsHad {
+
+    public static String checkNum(int number) {
+        int sum = 0;
+        String a = Integer.toString(number);
+        for (int i = 0; i < a.length(); i++) {
+            sum += a.charAt(i) - '0';
+        }
+        if (number % sum == 0) {
+            return "Harshad number";
+        } else {
+            return "Not Harshad number";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt(); 
+        reader.close();
+        System.out.println(HarsHad.checkNum(n));
+        System.out.println("sss");
+    }
+}
